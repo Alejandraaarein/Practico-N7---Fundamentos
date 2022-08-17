3142505aca7faf7b7095e4e628456785a15dd70e
Alejandraaarein <109768387+Alejandraaarein@users.noreply.github.com>
2022-08-16 22:02:54 -0300

    Update Ejercicios.js

--- Ejercicios.js
+++ Ejercicios.js
@@ -4,12 +4,29 @@ function Proto() {
     // Pista, utilizar: __proto__
     // Tu código:
 }
+var objeto={
+nombre:"objeto A"
+caracteristicas:"objeto de enseñanza"
+var "ale"
+nombre:"ale"
+característica:"principiante"
+ale-proto=objeto
+console.log(ale
+
 function agregarMetodoPrototype(Constructor) {
     // Agrega un método al Constructor del `prototype`
     // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
     // Tu código:
 
 }
+fuction usuario(nombre,apellido)
+this.nombre=nombre
+this.apellido=apellido
+usuario prototype saludar: fuction(){
+return"Hello world"
+var ale:new usuario("ale","reina")
+}
+
 
 function agregarStringInvertida() {
     // Agrega un método al prototype de String que devuelva la misma cadena de caracteres, pero invertida.
@@ -18,7 +35,13 @@ function agregarStringInvertida() {
     // 'joaco'.reverse() => 'ocaoj' 
     // Pista: Necesitarás usar "this" dentro de "reverse"
 
+}var persona={
+"florinda.reverse()=> florinda
+2esequiel".reverse()=> "leiuqese"
+metodo string: fuction(reverse) 
+this.reverse="leiuqese"
 }
+
 
 function changeNombre() {
     // Crea un objeto
@@ -27,6 +50,18 @@ function changeNombre() {
     // Mostrar por consola changeNombre() y getNombre(), con nombre distinto en changeNombre() al del constructor y al del getNombre()
     // Tu código:
 }
+var persona(){
+nombre "ale"
+apellido "reina"
+getnombre fuction (){
+returno this.nombre
+}
+changenombre fuction(){
+return this.nombre"patricia"
+}
+console.log("ptricia")
+
+
 
 function Object() {
     // Crea un objeto
@@ -34,3 +69,6 @@ function Object() {
     // Utilizar obligatoriamente el Metodo Object
     // Tu código:
 }
+    var= new object="peces"
+var= new object prototype "mar"
+console.log("mar")
